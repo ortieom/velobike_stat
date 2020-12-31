b50f0a009b8fa07ed42e4bd42732372b82986f42
ortieom <74673541+ortieom@users.noreply.github.com>
2020-12-31 10:47:05 +0500

    New structure in records

--- stat_collector_db.py
+++ stat_collector_db.py
@@ -22,10 +22,10 @@ for station in r['Items']:
     stations_in_response.append(station['Id'])
 
 
-def prepare_file_for_deltas(response, file, db):  # utility file
+def prepare_file_for_deltas(response, file):  # utility file
     create_new = False  # flag, true if file is new and nothing recorded
 
-    try:
+    try:  # inspecting
         with open(file, "r", encoding="utf-8", newline='') as csvfile:
             reader = csv.reader(csvfile, quotechar='"')
             if len(list(reader)) == 0:  # empty file
@@ -36,39 +36,23 @@ def prepare_file_for_deltas(response, file, db):  # utility file
     except FileNotFoundError:
         create_new = True
 
-    if create_new:
+    if create_new:  # creating if needed
         with open(file, "w", encoding="utf-8", newline='') as csvfile:
             writer = csv.writer(csvfile, quotechar='"')
 
-            writer.writerow(['Id', dt.datetime.now().strftime("%H:%M")])  # header
+            writer.writerow(['Id', dt.datetime.now().strftime("%d.%m.%y_%H:%M")])  # header
 
             for row in response:
-                total_free_places_now = row["FreeElectricPlaces"] + row["FreeOrdinaryPlaces"]
-                writer.writerow([row['Id'], total_free_places_now])
-
-    # preparing db
-    conn = sl.connect(db)
-    cursor = conn.cursor()
-    cursor.execute("SELECT velobike_id FROM collected_data ")
-    recorded_ids = cursor.fetchall()
-    if len(recorded_ids) == 0:
-        for row in response:
-            cursor.execute("INSERT INTO collected_data VALUES (?)", (row['Id'],))
-        conn.commit()
-    conn.close()
+                writer.writerow([row['Id'], row["FreeOrdinaryPlaces"], row["FreeElectricPlaces"]])
 
 
 def record_deltas(response, file, db):
     tempfile = NamedTemporaryFile('w+t', encoding="utf-8", newline='', delete=False)
 
-    current_time = dt.datetime.now().strftime('%H:%M')
+    current_time = dt.datetime.now().strftime("%d.%m.%y_%H:%M")
 
     conn = sl.connect(db)
     cursor = conn.cursor()
-    try:
-        cursor.execute(f"ALTER TABLE collected_data ADD COLUMN '{current_time}' INTEGER")
-    except sl.DatabaseError:
-        pass
 
     unused_stations_in_response = stations_in_response.copy()
 
@@ -80,37 +64,46 @@ def record_deltas(response, file, db):
             try:
                 unused_stations_in_response.pop(unused_stations_in_response.index(row[0]))
                 station_info = response[stations_in_response.index(row[0])]  # info for station by index of velobike_id
-                total_free_places_now = station_info["FreeElectricPlaces"] + station_info["FreeOrdinaryPlaces"]
 
                 if row[-1] != '-':  # delta for previous value
-                    delta = int(row[-1]) - total_free_places_now
+                    delta_ordinary = int(row[-2]) - station_info["FreeOrdinaryPlaces"]
+                    delta_electro = int(row[-1]) - station_info["FreeElectricPlaces"]
                 else:
-                    delta = 0
-                cursor.execute(f"UPDATE collected_data SET '{current_time}' = '{delta}' \n"
-                               f"WHERE velobike_id = '{row[0]}'")
+                    delta_ordinary = delta_electro = 0
 
-                row[-1] = total_free_places_now
+                cursor.execute('INSERT INTO collected_data '
+                               '(velobike_id, timestamp, ordinarybikes_available, electrobikes_available) '
+                               'VALUES (?, ?, ?, ?)',
+                               (row[0], current_time, delta_ordinary, delta_electro))
+
+                row[-1] = station_info["FreeElectricPlaces"]
+                row[-2] = station_info["FreeOrdinaryPlaces"]
+
                 writer.writerow(row)
 
             except ValueError:
                 if str(row[0]) == 'Id':  # updating header in utility file
                     row[-1] = current_time
                 else:  # not mentioned in response station (present in utility file)
-                    row[-1] = '-'
+                    row[-1] = row[-2] = '-'
 
-                    cursor.execute(f"UPDATE collected_data SET '{current_time}' = NULL \n"
-                                   f"WHERE velobike_id = '{row[0]}'")
+                    cursor.execute('INSERT INTO collected_data '
+                                   '(velobike_id, timestamp, ordinarybikes_available, electrobikes_available) '
+                                   'VALUES (?, ?, NULL, NULL)',
+                                   (row[0], current_time))
 
                 writer.writerow(row)
 
         for new_station in unused_stations_in_response:  # stations without previous records in utility file
             station_info = response[stations_in_response.index(new_station)]
 
-            cursor.execute(f"INSERT INTO collected_data (velobike_id, '{current_time}') VALUES (?, ?)",
-                           (station_info['Id'], 0))
+            cursor.execute('INSERT INTO collected_data '
+                           '(velobike_id, timestamp, ordinarybikes_available, electrobikes_available) '
+                           'VALUES (?, ?, 0, 0)',
+                           (station_info['Id'], current_time))
 
-            total_free_places_now = station_info["FreeElectricPlaces"] + station_info["FreeOrdinaryPlaces"]
-            new_station_row = [station_info['Id'], total_free_places_now]
+            new_station_row = [station_info['Id'],
+                               station_info["FreeOrdinaryPlaces"],  station_info["FreeElectricPlaces"]]
             writer.writerow(new_station_row)
 
     shutil.move(tempfile.name, file)
@@ -119,5 +112,5 @@ def record_deltas(response, file, db):
     conn.close()
 
 
-prepare_file_for_deltas(r['Items'], 'temp_records.csv', 'velobike.db')  # keeping utility file actual on start
+prepare_file_for_deltas(r['Items'], 'temp_records.csv')  # creating utility file on start if missing
 record_deltas(r['Items'], 'temp_records.csv', 'velobike.db')  # adding deltas
