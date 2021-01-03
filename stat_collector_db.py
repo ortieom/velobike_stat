a7891795996e8b1d52de03f29a46b69d57ad53df
ortieom <74673541+ortieom@users.noreply.github.com>
2021-01-03 22:23:20 +0500

    Recording available bikes (amount), using MySQL

--- stat_collector_db.py
+++ stat_collector_db.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from mysql.connector import MySQLConnection, Error
 from tempfile import NamedTemporaryFile
+from configparser import ConfigParser
 import datetime as dt
-import sqlite3 as sl
 import requests
 import warnings
 import shutil
 import json
 import csv
+import os
 
 # get response
 warnings.simplefilter('ignore', InsecureRequestWarning)
-r = requests.get('http://velobike.ru/ajax/parkings/', verify=False).text
+r = requests.get('http://194.58.122.45:6536/ajax/parkings/', verify=False).text
 r = r.replace('false', '0')
 r = r.replace('true', '1')
 r = json.loads(r)
@@ -20,6 +22,36 @@ r = json.loads(r)
 stations_in_response = []
 for station in r['Items']:
     stations_in_response.append(station['Id'])
+
+# logging preparations (26 - 43)
+log_filename = "stat_collector.log"
+
+if not os.path.exists(log_filename):
+    open(log_filename, 'w')
+
+
+def write_log(text):
+    timestamp = dt.datetime.strftime(dt.datetime.now(), "%Y-%m-%d %H:%M:%S")
+    with open(log_filename, 'a') as f:
+        f.write("{} {}".format(timestamp, text))
+        f.write("\n")
+
+
+def read_db_config(filename='config.ini', section='mysql'):
+    # create parser and read ini configuration file
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # get section, default to mysql
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db
 
 
 def prepare_file_for_deltas(response, file):  # utility file
@@ -30,7 +62,7 @@ def prepare_file_for_deltas(response, file):  # utility file
             reader = csv.reader(csvfile, quotechar='"')
             if len(list(reader)) == 0:  # empty file
                 create_new = True
-            for row in reader:   # row in utility file
+            for row in reader:  # row in utility file
                 if len(row) == 0 or len(row) == 1:  # confused string
                     create_new = True
     except FileNotFoundError:
@@ -40,19 +72,28 @@ def prepare_file_for_deltas(response, file):  # utility file
         with open(file, "w", encoding="utf-8", newline='') as csvfile:
             writer = csv.writer(csvfile, quotechar='"')
 
-            writer.writerow(['Id', dt.datetime.now().strftime("%d.%m.%y_%H:%M")])  # header
+            writer.writerow(['Id', dt.datetime.now().strftime("%Y-%m-%d %H:%M:00")])  # header
 
             for row in response:
                 writer.writerow([row['Id'], row["FreeOrdinaryPlaces"], row["FreeElectricPlaces"]])
 
 
-def record_deltas(response, file, db):
+def record_deltas(response, file):
     tempfile = NamedTemporaryFile('w+t', encoding="utf-8", newline='', delete=False)
 
-    current_time = dt.datetime.now().strftime("%d.%m.%y_%H:%M")
+    current_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:00")
 
-    conn = sl.connect(db)
-    cursor = conn.cursor()
+    db_config = read_db_config()
+    try:
+        conn = MySQLConnection(**db_config)
+        if not conn.is_connected():
+            print('connection failed')
+            return 0
+        cursor = conn.cursor()
+    except Error as error:
+        print(error)
+        write_log(error)
+        return 0
 
     unused_stations_in_response = stations_in_response.copy()
 
@@ -65,16 +106,11 @@ def record_deltas(response, file, db):
                 unused_stations_in_response.pop(unused_stations_in_response.index(row[0]))
                 station_info = response[stations_in_response.index(row[0])]  # info for station by index of velobike_id
 
-                if row[-1] != '-':  # delta for previous value
-                    delta_ordinary = int(row[-2]) - station_info["FreeOrdinaryPlaces"]
-                    delta_electro = int(row[-1]) - station_info["FreeElectricPlaces"]
-                else:
-                    delta_ordinary = delta_electro = 0
-
                 cursor.execute('INSERT INTO collected_data '
                                '(velobike_id, timestamp, ordinarybikes_available, electrobikes_available) '
-                               'VALUES (?, ?, ?, ?)',
-                               (row[0], current_time, delta_ordinary, delta_electro))
+                               'VALUES (%s, %s, %s, %s)',
+                               (row[0], current_time,
+                                station_info["AvailableOrdinaryBikes"], station_info["AvailableElectricBikes"]))
 
                 row[-1] = station_info["FreeElectricPlaces"]
                 row[-2] = station_info["FreeOrdinaryPlaces"]
@@ -89,7 +125,7 @@ def record_deltas(response, file, db):
 
                     cursor.execute('INSERT INTO collected_data '
                                    '(velobike_id, timestamp, ordinarybikes_available, electrobikes_available) '
-                                   'VALUES (?, ?, NULL, NULL)',
+                                   'VALUES (%s, %s, NULL, NULL)',
                                    (row[0], current_time))
 
                 writer.writerow(row)
@@ -99,18 +135,24 @@ def record_deltas(response, file, db):
 
             cursor.execute('INSERT INTO collected_data '
                            '(velobike_id, timestamp, ordinarybikes_available, electrobikes_available) '
-                           'VALUES (?, ?, 0, 0)',
-                           (station_info['Id'], current_time))
+                           'VALUES (%s, %s, %s, %s)',
+                           (row[0], current_time,
+                            station_info["AvailableOrdinaryBikes"], station_info["AvailableElectricBikes"]))
 
             new_station_row = [station_info['Id'],
-                               station_info["FreeOrdinaryPlaces"],  station_info["FreeElectricPlaces"]]
+                               station_info["FreeOrdinaryPlaces"], station_info["FreeElectricPlaces"]]
             writer.writerow(new_station_row)
 
     shutil.move(tempfile.name, file)
 
     conn.commit()
+    cursor.close()
     conn.close()
 
 
 prepare_file_for_deltas(r['Items'], 'temp_records.csv')  # creating utility file on start if missing
-record_deltas(r['Items'], 'temp_records.csv', 'velobike.db')  # adding deltas
+
+try:
+    record_deltas(r['Items'], 'temp_records.csv')  # adding deltas
+except:
+    write_log('records went wrong')
